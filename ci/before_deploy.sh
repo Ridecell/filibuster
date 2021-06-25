4067ce03484498b473c59297825a183a379bef09
Abhijat Malviya <abhijat@abhijat.io>
2021-06-25 18:43:21 +0530

    initial commit

---
+++ ci/before_deploy.sh
@@ -0,0 +1,27 @@
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+    cross rustc --bin chimpsky --target $TARGET --release -- -C lto
+    cp target/$TARGET/release/chimpsky $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::{fs, thread};
+use std::collections::HashMap;
+use std::convert::TryInto;
+use std::error::Error;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Configuration {
+    url: String,
+    headers: HashMap<String, String>,
+    query_params: HashMap<String, String>,
+    n_requests: u8,
+}
+
+fn query(configuration: Configuration) -> Result<(), Box<dyn Error>> {
+    let client = reqwest::blocking::Client::new();
+
+    loop {
+        let req = client.get(&configuration.url)
+            .headers((&configuration.headers).try_into()?);
+
+        let v: Vec<(&str, &str)> = configuration.query_params
+            .iter()
+            .map(|(k, v)| (k.as_ref(), v.as_ref()))
+            .collect();
+
+        println!("sending request");
+        let response = req.query(&v).send()?;
+        println!("{}", response.status());
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let config_file = "filibuster.json";
+
+    let configuration: Configuration = serde_json::from_str(&fs::read_to_string(config_file)
+        .expect("failed to read config file"))
+        .expect("failed to parse json");
+
+    let mut handles = vec![];
+    for _ in 0..configuration.n_requests {
+        let c = configuration.clone();
+        handles.push(thread::spawn(move || {
+            match query(c) {
+                Ok(_) => {}
+                Err(err) => println!("{}", err),
+            }
+        }));
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
+
+    Ok(())
+}
